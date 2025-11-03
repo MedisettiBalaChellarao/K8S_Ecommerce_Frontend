cd4de7293738dab15a05401225e0aeb99f870320
MEDISETTI BALA CHELLARAO <2300031585@kluniversity.in>
2025-11-03 09:13:22 +0530

    Update productService.js

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://ec2-98-89-1-73.compute-1.amazonaws.com:30083/back1/api/products";  // Updated Backend API URL
+const API_BASE_URL = "http://ec2-98-92-116-43.compute-1.amazonaws.com:30083/back1/api/products";  // Updated Backend API URL
 
 // Fetch products by category (computers, mobiles, laptops, pendrives, or all)
 export const getProducts = async (category = "") => {
